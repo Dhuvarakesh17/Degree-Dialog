5a34ca5ea34c222be6f11c74194ad706abe3539d
Mohammedihsaan28 <mohammedihsaan28@gmail.com>
2025-04-13 20:54:23 +0530

    Updated

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div className=''>
+        <header className='text-6xl text-blue-900 m-5  font-serif text-center  '>Degree Dialog</header>
+      </div>
+    </div>
+  )
+}
+
+export default App
